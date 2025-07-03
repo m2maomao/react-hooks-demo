594ad1f6f25433720768633f598ac08c63ebdf54
michael <m2maomao@163.com>
2025-07-03 22:55:28 +0800

    fix: useMemo缓存

--- src/NoEffect/MemoTodos/App.tsx
+++ src/NoEffect/MemoTodos/App.tsx
@@ -1,19 +1,32 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { initialTodos, createTodo, getVisibleTodos } from './todos.tsx';
+import { type Todo } from "./types/todos.types.ts";
 
 const TodoList = () => {
   // 所有数据
-  const [todos, setTodos] = useState(initialTodos);
+  const [todos, setTodos] = useState<Todo[]>(initialTodos);
   // 只显示未完成的事项
   const [showActive, setShowActive] = useState(false);
   // 新增的待办事项
   const [text, setText] = useState('');
-  // 可视的待办事项
-  const [visibleTodos, setVisibleTodos] = useState([]);
 
-  useEffect(() => {
-    setVisibleTodos(getVisibleTodos(todos, showActive));
+  // useMemo 版本测试
+  // 可视的待办事项
+  const visibleTodos = useMemo(() => {
+    console.time('useMemo caculation');
+    const result = (getVisibleTodos(todos, showActive));
+    console.timeEnd('useMemo caculation');
+    return result;
   }, [todos, showActive]);
+
+  // useEffect 版本测试
+  // const [visibleTodos, setVisibleTodos] = useState<Todo[]>([]);
+  // useEffect(() => {
+  //   console.time('useEffect caculation');
+  //   const result = (getVisibleTodos(todos, showActive));
+  //   console.timeEnd('useEffect caculation');
+  //   setVisibleTodos(result);
+  // }, [todos, showActive]);
 
   const handleAddClick = () => {
     setText('');
