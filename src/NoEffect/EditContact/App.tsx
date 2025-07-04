f87442e522591fb1840685fecc051b730d4e8267
michael <m2maomao@163.com>
2025-07-04 23:29:28 +0800

    feat: EditContact

---
+++ src/NoEffect/EditContact/App.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import ContactList from './ContactList';
+import EditContact from './EditContact';
+
+const initialContacts = [
+  { id: 0, name: 'Taylor', email: 'taylor@mail.com' },
+  { id: 1, name: 'Alice', email: 'alice@mail.com' },
+  { id: 2, name: 'Bob', email: 'bob@mail.com' },
+];
+
+const ContactManager = () => {
+  const [contacts, setContacts] = useState(initialContacts);
+  const [selectedId, setSelectedId] = useState(0);
+  const selectedContact = contacts.find(c => c.id === selectedId);
+
+  const handleSave = (updatedData) => {
+    const nextContacts = contacts.map(c => {
+      if (c.id === updatedData.id) {
+        return updatedData;
+      } else {
+        return c;
+      }
+    });
+    setContacts(nextContacts);
+  }
+
+  return (
+    <>
+      <ContactList
+        contacts={contacts}
+        selectedId={selectedId}
+        onSelect={id => setSelectedId(id)}
+      />
+      <hr />
+      <EditContact
+        savedContact={selectedContact}
+        onSave={handleSave}
+      />
+    </>
+  )
+}
+
+export default ContactManager;
